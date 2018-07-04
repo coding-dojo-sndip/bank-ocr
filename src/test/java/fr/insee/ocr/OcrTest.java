f6d37c2b3d10567d182ffd3c59377e019f949f92
Romain Warnan <romain.warnan@insee.fr>
2018-07-04 15:22:35 +0200

    Gestion de l'exception dans le test

--- src/test/java/fr/insee/ocr/OcrTest.java
+++ src/test/java/fr/insee/ocr/OcrTest.java
@@ -1,13 +1,16 @@
 package fr.insee.ocr;
 
 import static org.assertj.core.api.Assertions.*;
+
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class OcrTest {
 
 	
 	@Test
-	public void test_digits() {
+	public void test_digits() throws IOException {
 		String digits = Ocr.parse("digits.txt");
 		assertThat(digits).isEqualTo("0123456789");
 	}
